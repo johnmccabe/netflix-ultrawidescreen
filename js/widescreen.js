81927b782ee0380f2e6d095ec7c06d17575bae7c
John McCabe <john@johnmccabe.net>
2015-06-19 21:58:36 +0100

    Netflix May/June 2015 UI update support

--- js/widescreen.js
+++ js/widescreen.js
@@ -5,10 +5,11 @@ chrome.runtime.onInstalled.addListener(function() {
     // With a new rule
     chrome.declarativeContent.onPageChanged.addRules([
       {
-        // That fires when a page URL matches a WiPlayer page
+        // That fires when a page URL matches a watch page
         conditions: [
           new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { hostEquals: 'www.netflix.com', urlContains: 'WiPlayer' },
+            pageUrl: { hostEquals: 'www.netflix.com'},
+            css: ["#netflix-player"]
           })
         ],
         // Show the widescreen pageaction
@@ -27,7 +28,7 @@ chrome.pageAction.onClicked.addListener(function(tab){
     console.log('Enabling widescreen for ' + tab.url );
     // Resize the video wrapper to fill screen
     chrome.tabs.executeScript({
-      code: 'document.querySelector("#netflix-player > div.player-video-wrapper > div").style.cssText = "position: relative; width: 134%; height: 134%; margin-left:-585px; margin-top:-246px;"'
+      code: 'document.querySelector("#netflix-player > div.player-video-wrapper > div > video").style.cssText = "position: relative; width: 134%; height: 134%; margin-left:-585px; margin-top:-246px;"'
     });
     // Set page icon to active state
     chrome.pageAction.setIcon({path: "img/icon_on_19.png", tabId: tab.id});
@@ -36,7 +37,7 @@ chrome.pageAction.onClicked.addListener(function(tab){
     console.log('Disabling widescreen for ' + tab.url );
     // Restore original scaling
     chrome.tabs.executeScript({
-      code: 'document.querySelector("#netflix-player > div.player-video-wrapper > div").style.cssText = "position: relative; width: 100%; height: 100%; overflow: hidden;"'
+      code: 'document.querySelector("#netflix-player > div.player-video-wrapper > div > video").style.cssText = "position: relative; width: 100%; height: 100%; overflow: hidden;"'
     });
     // Set page icon to inactive state
     chrome.pageAction.setIcon({path: "img/icon_off_19.png", tabId: tab.id});
